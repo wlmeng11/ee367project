e614f5a7ea736376be955097b211e87744bc3d4e
William Meng <wlmeng11@gmail.com>
2021-03-14 16:11:01 -0700

    Physical vs simulated mask

--- Simulation/MeasureH.m
+++ Simulation/MeasureH.m
@@ -10,7 +10,7 @@ clearvars; clc; close all;
 field_init(-1)
 
 % Transducer Parameters (temporal)
-fc = 3e6; % Carrier Frequency [Hz]
+fc = 5e6; % Carrier Frequency [Hz]
 c = 1540; % Speed of Sound [m/s]
 lambda = c/fc; % Wavelength [m]
 fBW = 0.66; % Fractional Bandwidth
@@ -27,6 +27,7 @@ elevfoc = 20e-3; % Radius of elevation focus
 subx = 1; % Number of subdivisions for x elements
 suby = 5; % Number of subdivisions for y elements
 focus = [0 0 30e-3]; % Focal Point [m]
+x = -D/2:width:D/2;
 
 fprintf('Wavelength λ = %g mm\n', lambda*1e3);
 fprintf('Aperture size D = %g mm\n', D*1e3);
@@ -62,34 +63,35 @@ xdc_focus(Rx,0,focus);
 %% Generate pseudorandom delay mask
 rng('default'); s = rng; % set seed=0 for RNG
 
-% Delay mask
+% Delay mask as varying-thickness plastic layer
 c_plastic = 2750; % [m/s]
-sigma = 1/2 * lambda/c_plastic; % [s]
-delay_mask = sigma .* randn(tx_elem, 1); % Gaussian distribution
-
-figure(1);
-subplot(1, 2, 1);
-bar(delay_mask);
-axis square;
-xlabel('Element #');
-ylabel('Relative Delay (s)');
-title('Delay Mask (Simulation)');
-
-% Corresponding delay mask as varying-thickness plastic layer
-plastic_mask = c_plastic .* delay_mask;
+lambda_plastic = c_plastic/fc; % [m]
+sigma = lambda/2; % [m]
+plastic_mask = sigma .* randn(tx_elem, 1); % Gaussian distribution
 offset = -1.5 * min(plastic_mask);
 plastic_mask = plastic_mask + offset;
 
-subplot(1, 2, 2);
-bar(plastic_mask * 1e3);
-axis square;
-xlabel('x (a.u.)');
+figure(1);
+subplot(1, 2, 1);
+bar(x*1000, plastic_mask * 1e3);
+axis square tight;
+xlabel('x (mm)');
 ylabel('Mask Thickness (mm)');
 title('Plastic Mask (Physical)');
 
 set(gcf, 'Color', 'w');
 set(gcf, 'Position', [100 100 800 400]);
 saveas(gcf, 'Mask.png');
+
+% Delay mask as temporal delays for each transducer element
+delay_mask = plastic_mask ./ c_plastic; % Gaussian distribution
+
+subplot(1, 2, 2);
+bar(1:tx_elem, delay_mask * 1e9);
+axis square tight;
+xlabel('Element #');
+ylabel('Relative Delay (ns)');
+title('Delay Mask (Simulation)');
 
 
 % Set the Tx delays
